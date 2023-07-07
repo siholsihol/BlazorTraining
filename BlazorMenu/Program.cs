3ee10d99403c2eb74031cd852b594325b1364cd7
siholsihol <christiantosihol@hotmail.com>
2023-07-07 15:43:07 +0700

    add text editor

--- BlazorMenu/Program.cs
+++ BlazorMenu/Program.cs
@@ -2,7 +2,9 @@ using BlazorMenu.Extensions;
 using BlazorPrettyCode;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using R_BlazorFrontEnd;
 using R_BlazorFrontEnd.Controls.Extensions;
+using R_BlazorFrontEnd.FileConverter;
 using R_BlazorStartup;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -14,6 +16,7 @@ builder.Services.R_AddBlazorFrontEndControls();
 builder.R_RegisterBlazorServices();
 
 builder.Services.R_AddBlazorFrontEnd();
+builder.Services.AddTransient<R_IFileConverter, R_FileConverter>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazorPrettyCode();
