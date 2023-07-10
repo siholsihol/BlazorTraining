94e12b5ce82e02de0db578aec7011313c49d6d25
siholsihol <christiantosihol@hotmail.com>
2023-07-10 23:10:05 +0700

    drawer

--- BlazorMenu/Shared/MainLayout.razor.cs
+++ BlazorMenu/Shared/MainLayout.razor.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using BlazorMenu.Services;
+using BlazorMenu.Shared.Drawer;
+using BlazorMenu.Shared.Tabs;
+using Microsoft.AspNetCore.Components;
+using Telerik.Blazor.Components;
 
 namespace BlazorMenu.Shared
 {
@@ -11,5 +15,77 @@ namespace BlazorMenu.Shared
         {
             _iconMenuActive = iconMenuActive;
         }
+
+        private TelerikDrawer<DrawerMenuItem> _telerikDrawerRef;
+        private List<DrawerMenuItem> _data = new();
+        private bool Expanded = true;
+        private DrawerMenuItem _selectedItem;
+
+        [Inject] private R_IMenuService _menuService { get; set; }
+        [Inject] private MenuTabSetTool TabSetTool { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            var menuList = await _menuService.GetMenuAsync();
+
+            var menuIds = menuList.Where(x => x.CMENU_ID != "FAV")
+                .GroupBy(x => x.CMENU_ID)
+                .Select(x => x.First()).Select(x => x.CMENU_ID).ToArray();
+
+            _data = menuIds.Select(id => new DrawerMenuItem
+            {
+                Id = id,
+                Text = menuList.FirstOrDefault(x => x.CMENU_ID == id).CMENU_NAME,
+                Level = 0,
+                Children = menuList.Where(x => x.CSUB_MENU_TYPE == "G" && x.CMENU_ID == id).Select(y => new DrawerMenuItem
+                {
+                    Id = y.CSUB_MENU_ID,
+                    Text = y.CSUB_MENU_NAME,
+                    Level = 1,
+                    Children = menuList.Where(z => z.CSUB_MENU_TYPE == "P" && z.CPARENT_SUB_MENU_ID == y.CSUB_MENU_ID && z.CMENU_ID == id).Select(yy => new DrawerMenuItem
+                    {
+                        Id = yy.CSUB_MENU_ID,
+                        Text = yy.CSUB_MENU_NAME,
+                        Level = 2,
+                        Children = new()
+                    }).ToList()
+                }).ToList()
+            }).ToList();
+        }
+
+        private void OnItemSelect(DrawerMenuItem selectedItem)
+        {
+            _selectedItem = selectedItem;
+
+            if (selectedItem.Level == 2)
+            {
+                TabSetTool.AddTab(selectedItem.Text, selectedItem.Id, "A,U,D,P,V");
+                return;
+            }
+
+            selectedItem.Expanded = !selectedItem.Expanded;
+            var newData = new List<DrawerMenuItem>();
+
+            foreach (var item in _data.Where(x => x.Level <= selectedItem.Level))
+            {
+                newData.Add(item);
+                if (item == selectedItem && selectedItem.Expanded && (item.Children?.Any() ?? false))
+                {
+                    foreach (var child in item.Children)
+                    {
+                        newData.Add(child);
+                    }
+                }
+
+                if (item != selectedItem && !(item.Children?.Contains(selectedItem) ?? false))
+                {
+                    item.Expanded = false;
+                }
+            }
+
+            _data = newData;
+        }
+
+        private async Task ToggleDrawer() => await _telerikDrawerRef.ToggleAsync();
     }
 }
