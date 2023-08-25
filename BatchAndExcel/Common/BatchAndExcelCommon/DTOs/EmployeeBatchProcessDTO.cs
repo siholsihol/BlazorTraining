31b44c70df0dedf96461d247a3b9d643e914138f
siholsihol <christiantosihol@hotmail.com>
2023-08-25 17:57:39 +0700

    add batch

---
+++ BatchAndExcel/Common/BatchAndExcelCommon/DTOs/EmployeeBatchProcessDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace BatchAndExcelCommon.DTOs
+{
+    public class EmployeeBatchProcessDTO
+    {
+        public int SeqNo { get; set; }
+        public string CompanyId { get; set; }
+        public string EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SexId { get; set; }
+        public int TotalChildren { get; set; }
+    }
+}
