5012d04a5a837bace03e20fc9f402de68ac120e2
siholsihol <christiantosihol@hotmail.com>
2024-01-04 19:49:57 +0700

    update excel

--- BatchAndExcel/Common/BatchAndExcelCommon/DTOs/EmployeeDTO.cs
+++ BatchAndExcel/Common/BatchAndExcelCommon/DTOs/EmployeeDTO.cs
@@ -1,9 +1,13 @@
-﻿namespace BatchAndExcelCommon.DTOs
+﻿using System;
+
+namespace BatchAndExcelCommon.DTOs
 {
     public class EmployeeDTO
     {
         public string Id { get; set; }
         public string FirstName { get; set; }
         public int Gender { get; set; }
+        public string HireDate { get; set; }
+        public DateTime DHIRE_DATE { get; set; }
     }
 }
