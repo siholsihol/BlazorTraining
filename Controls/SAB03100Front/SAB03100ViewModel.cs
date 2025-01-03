09c2eb8ed9a714ad53297f90870d84edab9a2225
siholsihol <christiantosihol@hotmail.com>
2025-01-03 18:18:21 +0700

    add stepper example

---
+++ Controls/SAB03100Front/SAB03100ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using R_BlazorFrontEnd;
+
+namespace SAB03100Front
+{
+    public class SAB03100ViewModel : R_ViewModel<EmployeeDTO>
+    {
+        public EmployeeDTO Employee = new EmployeeDTO();
+    }
+
+    public class EmployeeDTO
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public int Gender { get; set; }
+    }
+}
