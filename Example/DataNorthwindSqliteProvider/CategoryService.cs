1dd354d057b3aed9b37fd8733b7df03b50dd0f3c
siholsihol <christiantosihol@hotmail.com>
2025-09-17 17:55:42 +0700

    add sqlite dan sql server

---
+++ Example/DataNorthwindSqliteProvider/CategoryService.cs
@@ -0,0 +1,115 @@
+﻿using Dapper;
+using DataNorthwindSqliteProvider.Helper;
+using DataProvider.DTOs;
+using DataProvider.Services;
+
+namespace DataNorthwindSqliteProvider
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly DataContext _dataContext;
+
+        public CategoryService(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task CreateCategoryAsync(CategoryDTO itemToAdd)
+        {
+            try
+            {
+                using var connection = _dataContext.CreateConnection();
+
+                var sql = "INSERT INTO Categories (CategoryName, Description) VALUES (@CategoryName, @Description)";
+                var affectedRows = await connection.ExecuteAsync(sql,
+                    new
+                    {
+                        CategoryName = itemToAdd.Name,
+                        Description = itemToAdd.Description
+                    });
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task UpdateCategoryAsync(CategoryDTO itemToUpdate)
+        {
+            try
+            {
+                using var connection = _dataContext.CreateConnection();
+
+                var sql = "UPDATE Categories SET CategoryName = @CategoryName, Description = @Description WHERE CategoryID = @CategoryID";
+                var affectedRows = await connection.ExecuteAsync(sql,
+                    new
+                    {
+                        CategoryName = itemToUpdate.Name,
+                        Description = itemToUpdate.Description,
+                        CategoryID = itemToUpdate.Id
+                    });
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task DeleteCategoryAsync(CategoryDTO itemToDelete)
+        {
+            try
+            {
+                using var connection = _dataContext.CreateConnection();
+
+                var sql = "DELETE FROM Categories WHERE CategoryID = @CategoryID";
+                var affectedRows = await connection.ExecuteAsync(sql,
+                    new
+                    {
+                        CategoryID = itemToDelete.Id
+                    });
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<CategoryDTO>> GetCategoriesAsync()
+        {
+            try
+            {
+                using var connection = _dataContext.CreateConnection();
+
+                var sql = "SELECT * FROM Categories";
+                var customers = await connection.QueryAsync<CategoryDTO>(sql);
+
+                return customers.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<CategoryDTO> GetCategoryAsync(int categoryId)
+        {
+            try
+            {
+                using var connection = _dataContext.CreateConnection();
+
+                var sql = "SELECT * FROM Categories WHERE CategoryID = @CategoryID";
+                var customer = await connection.QueryFirstOrDefaultAsync<CategoryDTO>(sql,
+                    new
+                    {
+                        CategoryID = categoryId
+                    });
+
+                return customer;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Example/DataNorthwindSqliteProvider/Helper/DataContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Reflection;
+
+namespace DataNorthwindSqliteProvider.Helper
+{
+    public class DataContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            var resourceName = "DataNorthwindSqliteProvider.Northwind.db";
+            var tempFileName = Path.GetTempFileName();
+            using var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)
+              ?? throw new FileNotFoundException($"Embedded resource {resourceName} not found.");
+
+            using var tempStream = File.Create(tempFileName);
+
+            // We are using a buffer to efficiently copy possibly large files.
+            const int bufferSize = 8 * 1024; // 8 KB buffer (adjust as needed)
+            var buffer = new byte[bufferSize];
+
+            int bytesRead;
+            while ((bytesRead = resourceStream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                tempStream.Write(buffer, 0, bytesRead);
+            }
+
+            var conStr = _configuration.GetConnectionString($"Data Source={tempFileName}");
+            var tempConnection = new SqliteConnection(conStr);
+            tempConnection.Open();
+
+            var connection = new SqliteConnection("Data Source=:memory:");
+            connection.Open();
+
+            tempConnection.BackupDatabase(connection);
+
+            tempConnection.Close();
+            File.Delete(tempFileName);
+
+            return connection;
+        }
+    }
+}
