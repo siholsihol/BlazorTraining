82537ab0368f1e4f15382bbed359ba2857a753bb
siholsihol <christiantosihol@hotmail.com>
2025-09-19 15:45:28 +0700

    add excel view model

--- BatchAndExcel/Front/BatchAndExcel/Pages/Excel/Excel.razor.cs
+++ BatchAndExcel/Front/BatchAndExcel/Pages/Excel/Excel.razor.cs
@@ -1,16 +1,13 @@
-﻿using BatchAndExcelCommon.DTOs;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.JSInterop;
 using R_BlazorFrontEnd;
 using R_BlazorFrontEnd.Controls;
 using R_BlazorFrontEnd.Controls.Enums;
 using R_BlazorFrontEnd.Exceptions;
-using R_BlazorFrontEnd.Helpers;
-using System.Collections.ObjectModel;
 using Telerik.Blazor;
 
-namespace BatchAndExcel.Pages
+namespace BatchAndExcel.Pages.Excel
 {
     public partial class Excel
     {
@@ -18,8 +15,9 @@ namespace BatchAndExcel.Pages
         [Inject] private R_IExcel ExcelProvider { get; set; }
         [Inject] private IJSRuntime JSRuntime { get; set; }
 
-        private R_eFileSelectAccept[] accepts = { R_eFileSelectAccept.Excel };
-        private ObservableCollection<EmployeeDTO> _gridList = new();
+        private ExcelViewModel _excelViewModel = new();
+        private R_eFileSelectAccept[] _accepts = { R_eFileSelectAccept.Excel };
+        //private ObservableCollection<EmployeeDTO> _gridList = new();
 
         #region FILE SELECT
 
@@ -37,14 +35,7 @@ namespace BatchAndExcel.Pages
                 //import from excel
                 var loDataSet = ExcelProvider.R_ReadFromExcel(loByteFile, new string[] { "Employee" });
 
-                var loResultEmployee = R_FrontUtility.R_ConvertTo<EmployeeDTO>(loDataSet.Tables[0]);
-
-                foreach (var loEmployee in loResultEmployee)
-                {
-                    loEmployee.DHIRE_DATE = R_FrontUtility.R_ConvertToDateTime(loEmployee.HireDate, "yyyyMMdd");
-                }
-
-                _gridList = new ObservableCollection<EmployeeDTO>(loResultEmployee);
+                _excelViewModel.SetEmployeeListFromDataSet(loDataSet);
             }
             catch (Exception ex)
             {
@@ -61,14 +52,9 @@ namespace BatchAndExcel.Pages
 
             try
             {
-                var loEmployeeList = new List<EmployeeDTO>()
-            {
-                new EmployeeDTO() {Id = Guid.NewGuid().ToString(), FirstName="Sihol", Gender="M", HireDate = "20250917"},
-                new EmployeeDTO() {Id = Guid.NewGuid().ToString(), FirstName="Siholwati", Gender="F", HireDate = "20250917"}
-            };
-
-                var loDataTable = R_FrontUtility.R_ConvertTo(loEmployeeList);
-                loDataTable.TableName = "Employee";
+                var loDataTable = _excelViewModel.CreateDataTableEmployee();
+                if (loDataTable is null)
+                    return;
 
                 //export to excel
                 var loByteFile = ExcelProvider.R_WriteToExcel(loDataTable);
---
+++ BatchAndExcel/Front/BatchAndExcel/Pages/Excel/ExcelViewModel.cs
@@ -0,0 +1,62 @@
+﻿using BatchAndExcelCommon.DTOs;
+using R_BlazorFrontEnd.Exceptions;
+using R_BlazorFrontEnd.Helpers;
+using System.Collections.ObjectModel;
+using System.Data;
+
+namespace BatchAndExcel.Pages.Excel
+{
+    public class ExcelViewModel
+    {
+        public ObservableCollection<EmployeeDTO> EmployeeList { get; private set; }
+
+        public void SetEmployeeListFromDataSet(DataSet poDataSetEmployee)
+        {
+            var loEx = new R_Exception();
+
+            try
+            {
+                var loResultEmployee = R_FrontUtility.R_ConvertTo<EmployeeDTO>(poDataSetEmployee.Tables[0]);
+
+                foreach (var loEmployee in loResultEmployee)
+                {
+                    loEmployee.DHIRE_DATE = R_FrontUtility.R_ConvertToDateTime(loEmployee.HireDate, "yyyyMMdd");
+                }
+
+                EmployeeList = new ObservableCollection<EmployeeDTO>(loResultEmployee);
+            }
+            catch (Exception ex)
+            {
+                loEx.Add(ex);
+            }
+
+            loEx.ThrowExceptionIfErrors();
+        }
+
+        public DataTable CreateDataTableEmployee()
+        {
+            var loEx = new R_Exception();
+            DataTable loDataTable = null;
+
+            try
+            {
+                var loEmployeeList = new List<EmployeeDTO>()
+            {
+                new EmployeeDTO() {Id = Guid.NewGuid().ToString(), FirstName="Sihol", Gender="M", HireDate = "20250917"},
+                new EmployeeDTO() {Id = Guid.NewGuid().ToString(), FirstName="Siholwati", Gender="F", HireDate = "20250917"}
+            };
+
+                loDataTable = R_FrontUtility.R_ConvertTo(loEmployeeList);
+                loDataTable.TableName = "Employee";
+            }
+            catch (Exception ex)
+            {
+                loEx.Add(ex);
+            }
+
+            loEx.ThrowExceptionIfErrors();
+
+            return loDataTable;
+        }
+    }
+}
