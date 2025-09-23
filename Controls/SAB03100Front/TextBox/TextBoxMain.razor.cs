b7a189f937a5f72e3fa708838f1b382c17509f8a
siholsihol <christiantosihol@hotmail.com>
2025-09-23 16:05:46 +0700

    add textbox property

---
+++ Controls/SAB03100Front/TextBox/TextBoxMain.razor.cs
@@ -0,0 +1,51 @@
+﻿using BlazorTraining.Controls.Shared;
+using Microsoft.AspNetCore.Components;
+using R_BlazorFrontEnd.Controls.Popup;
+
+namespace SAB03100Front.TextBox
+{
+    public partial class TextBoxMain
+    {
+        [Inject] private R_PopupService PopupService { get; set; }
+
+        private async Task AllowSpacesOnClick()
+        {
+            await PopupService.Show(
+                typeof(DemoPopup<TextBox_Demo_AllowSpaces>),
+                null,
+                poPopupSettings: new R_PopupSettings { PageTitle = "TextBox AllowSpaces" });
+        }
+
+        private async Task AllowSpecialCharactersOnClick()
+        {
+            await PopupService.Show(
+                typeof(DemoPopup<TextBox_Demo_AllowSpecialCharacters>),
+                null,
+                poPopupSettings: new R_PopupSettings { PageTitle = "TextBox AllowSpecialCharacters" });
+        }
+
+        private async Task CharacterCasingOnClick()
+        {
+            await PopupService.Show(
+                typeof(DemoPopup<TextBox_Demo_CharacterCasing>),
+                null,
+                poPopupSettings: new R_PopupSettings { PageTitle = "TextBox CharacterCasing" });
+        }
+
+        private async Task EnabledOnClick()
+        {
+            await PopupService.Show(
+                typeof(DemoPopup<TextBox_Demo_Enabled>),
+                null,
+                poPopupSettings: new R_PopupSettings { PageTitle = "TextBox Enabled" });
+        }
+
+        private async Task MaxLengthOnClick()
+        {
+            await PopupService.Show(
+                typeof(DemoPopup<TextBox_Demo_MaxLength>),
+                null,
+                poPopupSettings: new R_PopupSettings { PageTitle = "TextBox MaxLength" });
+        }
+    }
+}
