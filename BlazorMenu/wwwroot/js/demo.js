e360ff3e1fc0b41bf02f8eafdf352df2704daab8
Natasha Djayaputera <natashadjayaputera@gmail.com>
2026-01-14 09:30:59 +0700

    add fade in tab content

--- BlazorMenu/wwwroot/js/demo.js
+++ BlazorMenu/wwwroot/js/demo.js
@@ -11,3 +11,25 @@ window.highlightCode = function () {
         Prism.highlightAll();
     }
 };
+
+window.initScrollFades = () => {
+    document.querySelectorAll('.doc-scroll-content').forEach(el => {
+        // If already initialized, skip
+        if (el._scrollFadeInitialized) return;
+
+        const wrapper = el.closest('.doc-scroll-wrapper');
+
+        const update = () => {
+            const show = el.scrollTop + el.clientHeight < el.scrollHeight - 2;
+            wrapper.style.setProperty('--fade-bottom', show ? '1' : '0');
+        };
+
+        el.addEventListener('scroll', update);
+        new ResizeObserver(update).observe(el);
+
+        update(); // initial
+
+        // Mark as initialized
+        el._scrollFadeInitialized = true;
+    });
+};
